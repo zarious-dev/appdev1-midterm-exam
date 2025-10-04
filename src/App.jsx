aa11c90d9ba55dc370154d7b0b6b7e979a1d9d2b
zarious-dev <enriquez.caezarie@gmail.com>
2025-10-04 12:33:01 +0800

    Deleted preloader

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,6 @@ function App() {
 
   return (
     <>
-      {/* ***** Preloader Start ***** */}
-      <div id="js-preloader" className="js-preloader">
-        <div className="preloader-inner">
-          <span className="dot" />
-          <div className="dots">
-            <span />
-            <span />
-            <span />
-          </div>
-        </div>
-      </div>
-      {/* ***** Preloader End ***** */}
       {/* ***** Header Area Start ***** */}
       <header
         className="header-area header-sticky wow slideInDown"
